292c9f8cf5e2d2739ee169e1d0b9952136a78274
nicsylvia <nicsylvia15f@gmail.com>
2023-04-06 14:45:20 -0700

    saved malam and agent details

--- src/Interfaces/AllInterfaces.ts
+++ src/Interfaces/AllInterfaces.ts
@@ -9,3 +9,27 @@ export interface UserDetails {
   confirmPassword: string;
   dateTime: string;
 }
+
+export interface AgentDetails {
+  name: string;
+  email: string;
+  phoneNumber: number;
+  LGA: string;
+  address: string;
+  password: string;
+  confirmPassword: string;
+  dateTime: string;
+}
+
+export interface MalamDetails {
+  name: string;
+  email: string;
+  image: string;
+  uniqueID: string;
+  phoneNumber: number;
+  LGA: string;
+  address: string;
+  password: string;
+  confirmPassword: string;
+  dateTime: string;
+}
