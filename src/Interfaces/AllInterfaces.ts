a7d218b18a31058b99df629d2f4cb0df05cda445
nicsylvia <nicsylvia15f@gmail.com>
2023-04-06 14:38:43 -0700

    saved user interfaces

---
+++ src/Interfaces/AllInterfaces.ts
@@ -0,0 +1,11 @@
+export interface UserDetails {
+  name: string;
+  username: string;
+  email: string;
+  phoneNumber: number;
+  LGA: string;
+  address: string;
+  password: string;
+  confirmPassword: string;
+  dateTime: string;
+}
