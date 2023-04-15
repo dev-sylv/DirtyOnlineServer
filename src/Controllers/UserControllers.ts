2d52f91a0a24fae0841601d37167d06d19817767
nicsylvia <nicsylvia15f@gmail.com>
2023-04-14 17:45:30 -0700

    saved user controllers

--- src/Controllers/UserControllers.ts
+++ src/Controllers/UserControllers.ts
@@ -6,17 +6,16 @@ import { MainAppError, HTTPCODES } from "../Utils/MainAppError";
 import StationModels from "../Models/StationModels";
 import RequestModels from "../Models/RequestModels";
 import mongoose from "mongoose";
-import DirectorModels from "../Models/ManagerModels";
 
 // Users Registration:
 export const UsersRegistration = AsyncHandler(
   async (req: any, res: Response, next: NextFunction) => {
-    const { name, address, phoneNumber, password, stationName } = req.body;
+    const { name, address, email, password, stationName } = req.body;
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    const findUser = await UserModels.findOne({ phoneNumber });
+    const findUser = await UserModels.findOne({ email });
 
     if (findUser) {
       next(
@@ -31,9 +30,9 @@ export const UsersRegistration = AsyncHandler(
     if (FindStation) {
       const users = await UserModels.create({
         name,
+        email,
         role: "User",
         address,
-        phoneNumber,
         password: hashedPassword,
         station: FindStation,
         numberOfRequests: 4,
@@ -60,16 +59,21 @@ export const UsersRegistration = AsyncHandler(
 // Users Login:
 export const UsersLogin = AsyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    const { phoneNumber, password } = req.body;
-    if (!phoneNumber)
+    const { email, password } = req.body;
+    if (!email)
       next(
         new MainAppError({
           httpcode: HTTPCODES.BAD_REQUEST,
-          message: "Please input your phone number",
+          message: "Please input your email address",
         })
       );
     const user = await UserModels.findOne({
-      phoneNumber,
+      email,
+    }).populate({
+      path: "station",
+      options: {
+        createdAt: -1,
+      },
     });
     if (!user)
       next(
@@ -89,7 +93,7 @@ export const UsersLogin = AsyncHandler(
       next(
         new MainAppError({
           httpcode: HTTPCODES.NOT_FOUND,
-          message: "Phone number or password not correct",
+          message: "Email or password not correct",
         })
       );
     }
@@ -99,7 +103,12 @@ export const UsersLogin = AsyncHandler(
 // Get all Users:
 export const GetAllUsers = AsyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    const users = await UserModels.find();
+    const users = await UserModels.find().populate({
+      path: "station",
+      options: {
+        createdAt: -1,
+      },
+    });
 
     if (!users) {
       next(
@@ -120,7 +129,12 @@ export const GetAllUsers = AsyncHandler(
 // Get a single User:
 export const GetSingleUser = AsyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    const singleuser = await UserModels.findById(req.params.userID);
+    const singleuser = await UserModels.findById(req.params.userID).populate({
+      path: "station",
+      options: {
+        createdAt: -1,
+      },
+    });
 
     if (!singleuser) {
       next(
@@ -142,65 +156,70 @@ export const GetSingleUser = AsyncHandler(
 export const UserMakesARequest = AsyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     // Get the user:
-    const getUser = await UserModels.findById(req.params.userID);
+    const getUser = await UserModels.findById(req.params.userID).populate({
+      path: "station",
+      options: {
+        createdAt: -1,
+      },
+    });
+    const GetUserStation = getUser?.station;
+    console.log("User station: ", GetUserStation);
 
     if (getUser) {
-      // If user can still make requests
-      if (getUser!.numberOfRequests <= 4) {
-        const GetUserStation = getUser?.station;
-        // User makes the requests:
-        const Time = new Date();
-        const DisposewasteRequests = await RequestModels.create({
-          requestMessage: `${getUser?.name} who resides at ${getUser?.address} made a request by ${Time} for a waste disposal`,
-          requestStatus: true,
-        });
-        // Get the station the user is apportioned to and push the created request into it:
-        getUser?.makeRequests.push(
-          new mongoose.Types.ObjectId(DisposewasteRequests?._id)
-        );
-        getUser?.save();
+      // Check if user station is in all the stations we have in the database
+      const CheckUserStation = await StationModels.findOne({
+        GetUserStation,
+      });
 
-        // Check if user station is in all the stations we have in the database
-        const CheckUserStation = await StationModels.findOne({
-          GetUserStation,
-        });
-        // If the station exists, push the requests to the station to notify them:
-        if (CheckUserStation) {
+      if (CheckUserStation) {
+        // If user can still make requests
+        if (getUser!.numberOfRequests <= 4) {
+          // User makes the requests:
+          const Time = new Date().toString();
+          const DisposewasteRequests = await RequestModels.create({
+            requestMessage: `${getUser?.name} who resides at ${getUser?.address} made a request by ${Time} for a waste disposal`,
+            requestStatus: true,
+          });
+          // Get the station the user is apportioned to and push the created request into it:
+          getUser?.makeRequests.push(
+            new mongoose.Types.ObjectId(DisposewasteRequests?._id)
+          );
+          getUser?.save();
+
+          // If the station exists, push the requests to the station to notify them:
           CheckUserStation?.requests.push(
             new mongoose.Types.ObjectId(DisposewasteRequests?._id)
           );
 
+          // Update the decrement of the user no of requests remaining:
+          const DecreaseRequests = await UserModels.findByIdAndUpdate(
+            req.params.userID,
+            {
+              numberOfRequests: getUser?.numberOfRequests - 1,
+            },
+            { new: true }
+          );
           return res.status(HTTPCODES.OK).json({
-            message: `Dear ${getUser?.name}, your requests has been sent to your station @${GetUserStation}`,
+            message: "Request sent successfully",
+            data: DisposewasteRequests,
+            RemainingRequest: `Your requests for this month is remaining ${getUser?.numberOfRequests} `,
+            RequestData: DecreaseRequests,
+            RequestNotification: `Dear ${getUser?.name}, your requests has been sent to your station @${GetUserStation}`,
           });
         } else {
+          // If the no of request is more than 4
           next(
             new MainAppError({
-              message: "This station does not exist",
-              httpcode: HTTPCODES.NOT_FOUND,
+              message: "You can't make any other requests till next month",
+              httpcode: HTTPCODES.BAD_REQUEST,
             })
           );
         }
-        // Update the decrement of the user no of requests remaining:
-        const DecreaseRequests = await UserModels.findByIdAndUpdate(
-          req.params.userID,
-          {
-            numberOfRequests: getUser?.numberOfRequests - 1,
-          },
-          { new: true }
-        );
-        return res.status(HTTPCODES.OK).json({
-          message: "Request sent successfully",
-          data: DisposewasteRequests,
-          RemainingRequest: `Your requests for this month is remaining ${getUser?.numberOfRequests} `,
-          RequestData: DecreaseRequests,
-        });
       } else {
-        // If the no of request is more than 4
         next(
           new MainAppError({
-            message: "You can't make any other requests till next month",
-            httpcode: HTTPCODES.BAD_REQUEST,
+            message: "This station does not exist",
+            httpcode: HTTPCODES.NOT_FOUND,
           })
         );
       }
