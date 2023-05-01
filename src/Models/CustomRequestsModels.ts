9008f483db8e904648e1da5dfcbc570fac629a1d
nicsylvia <nicsylvia15f@gmail.com>
2023-05-01 15:01:08 -0700

    working on custom requests

---
+++ src/Models/CustomRequestsModels.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, model } from "mongoose";
+
+import { requestInterface } from "../Interfaces/AllInterfaces";
+
+const RequestSchema = new Schema(
+  {
+    requestMessage: {
+      type: String,
+    },
+    requestStatus: {
+      type: Boolean,
+      default: false,
+    },
+    assigned: {
+      type: Boolean,
+      default: false,
+    },
+    DoneBy: {
+      type: String,
+    },
+    Pending: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const RequestModels = model<requestInterface>("Requests", RequestSchema);
+
+export default RequestModels;
