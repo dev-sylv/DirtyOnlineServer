b02cacfa3e5010765bd7eccbfbd95b6f25d70184
nicsylvia <nicsylvia15f@gmail.com>
2023-05-11 17:11:20 -0700

    updated environment variables

--- src/Config/EnvironmentVariables.ts
+++ src/Config/EnvironmentVariables.ts
@@ -5,4 +5,7 @@ dotenv.config();
 export const EnvironmentVariables = {
   PORT: process.env.PORT as string,
   MONGODB_STRING: process.env.LIVE_URL as string,
+  GOOGLE_ID: process.env.GOOGLE_ID as string,
+  GOOGLE_SECRET: process.env.GOOGLE_SECRET as string,
+  GOOGLE_REFRESHTOKEN: process.env.GOOGLE_REFRESHTOKEN as string,
 };
