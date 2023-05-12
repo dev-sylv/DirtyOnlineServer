3f7b993ffc892f61f4e38ad95f771f9250a1aaf7
nicsylvia <nicsylvia15f@gmail.com>
2023-05-11 17:28:46 -0700

    remodified db connect

--- src/Config/Database.ts
+++ src/Config/Database.ts
@@ -7,9 +7,15 @@ const LIVEURI = EnvironmentVariables.MONGODB_STRING;
 
 export const DBCONNECTION = async () => {
   try {
-    const conn = await mongoose.connect(LIVEURI);
-    console.log("");
-    console.log(`Database is connected to ${conn.connection.host}`);
+    if (LIVEURI) {
+      const conn = await mongoose.connect(LIVEURI);
+      console.log("");
+      console.log(`Database is connected to ${conn.connection.host}`);
+    } else {
+      const conn = await mongoose.connect(db_Url);
+      console.log("");
+      console.log(`Database is connected to ${conn.connection.host}`);
+    }
   } catch (error) {
     console.log("An error occured in connecting to DB");
   }
