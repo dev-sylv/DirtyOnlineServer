022a814220bb797713cb6e151fd36517e4c77320
nicsylvia <nicsylvia15f@gmail.com>
2023-05-11 18:21:41 -0700

    working on verification ejs

--- src/Config/Database.ts
+++ src/Config/Database.ts
@@ -1,21 +1,24 @@
 import mongoose from "mongoose";
 import { EnvironmentVariables } from "./EnvironmentVariables";
 
-const db_Url = "mongodb://localhost/MalamOnline";
+const db_Url = "mongodb://localhost:27017/MalamOnline";
 
 const LIVEURI = EnvironmentVariables.MONGODB_STRING;
 
 export const DBCONNECTION = async () => {
   try {
-    if (LIVEURI) {
-      const conn = await mongoose.connect(LIVEURI);
-      console.log("");
-      console.log(`Database is connected to ${conn.connection.host}`);
-    } else {
-      const conn = await mongoose.connect(db_Url);
-      console.log("");
-      console.log(`Database is connected to ${conn.connection.host}`);
-    }
+    const conn = await mongoose.connect(db_Url);
+    console.log("");
+    console.log(`Database is connected to ${conn.connection.host}`);
+    // if (LIVEURI) {
+    //   const conn = await mongoose.connect(LIVEURI);
+    //   console.log("");
+    //   console.log(`Database is connected to ${conn.connection.host}`);
+    // } else {
+    //   const conn = await mongoose.connect(db_Url);
+    //   console.log("");
+    //   console.log(`Database is connected to ${conn.connection.host}`);
+    // }
   } catch (error) {
     console.log("An error occured in connecting to DB");
   }
